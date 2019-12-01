c26039cb964cd080dd4f2b998e763726d813fa68
Mário Cosme <mariocosme@gmail.com>
2019-12-01 11:30:33 +0000

    Fix images being cut out

--- app/src/main/java/pt/cosmik/boostctrl/ui/common/BoostCtrlSmallViewPagerAdapter.kt
+++ app/src/main/java/pt/cosmik/boostctrl/ui/common/BoostCtrlSmallViewPagerAdapter.kt
@@ -1,5 +1,6 @@
 package pt.cosmik.boostctrl.ui.common
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -9,12 +10,13 @@ import androidx.viewpager.widget.PagerAdapter
 import com.bumptech.glide.Glide
 import pt.cosmik.boostctrl.R
 
-class BoostCtrlSmallViewPagerAdapter(val context: Context, val images: List<String> ): PagerAdapter() {
+class BoostCtrlSmallViewPagerAdapter(val context: Context, private val images: List<String> ): PagerAdapter() {
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean = `object` == view
 
     override fun getCount(): Int = images.size
 
+    @SuppressLint("InflateParams")
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
         val view = LayoutInflater.from(context).inflate(R.layout.small_image_view_pager_item, null)
         val imageView = view.findViewById<ImageView>(R.id.image)
